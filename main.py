9722e49ef85c4d7e42f6328d4105a68bda12cda4
Hossam Al-Dokkani <hossam.o@trufla.com>
2018-11-23 00:33:37 +0200

    Solution #1

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-# Add python code in this file
+import csv
+
+with open('cities.csv') as f:
+    cities = [city for city in csv.DictReader(f)]
+
+with open('points.csv') as f:
+    points = []
+    for point in csv.DictReader(f):
+        for city in cities:
+            city_xrange = range(int(city['TopLeft_X']), int(city['BottomRight_X']) + 1)
+            city_yrange = range(int(city['TopLeft_Y']), int(city['BottomRight_Y']) + 1)
+            if int(point['X']) in city_xrange and int(point['Y']) in city_yrange:
+                point['City'] = city['Name']
+                break
+        if point.get('City') is None:
+            point['City'] = 'None'
+        points.append(point)
+
+with open('output_points.csv', 'w') as f:
+    fieldnames = ['ID', 'X', 'Y', 'City']
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerows(points)
